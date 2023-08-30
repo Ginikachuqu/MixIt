505f6456c36a8b1534c69244ebe7c18cb4299da2
Charles Grandison <therealakeem03@gmail.com>
2023-08-30 15:58:33 +0100

    Finalized styling on recipe display page

--- src/pages/recipe display/Recipe.styles.tsx
+++ src/pages/recipe display/Recipe.styles.tsx
@@ -1,132 +1,441 @@
 import { styled } from "styled-components";
 
 export const Wrapper = styled.section`
-    padding-top: 4.5rem;
-    width: 100%;
+  padding-top: 4.5rem;
+  width: 100%;
 
-    .hero {
-        width: 90%;
-        height: 500px;
-        /* background: #000; */
-        margin: 0 auto;
+  .hero {
+    width: 90%;
+    height: 500px;
+    /* background: #000; */
+    margin: 0 auto;
 
-        &__inner {
-            display: flex;
-            height: 100%;
-            
-            &-left {
-                width: 50%;
-                display: flex;
-                flex-direction: column;
-                justify-content: center;
-                align-items: center;
-                gap: 1rem;
+    &__inner {
+      display: flex;
+      height: 100%;
 
-                .badge {
-                    display: flex;
-                    align-items: center;
-                    gap: .5rem;
-                    font-size: 1.5rem;
+      &-left {
+        width: 50%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        gap: 1rem;
 
-                    svg {
-                        color: green;
-                    }
+        .badge {
+          display: flex;
+          align-items: center;
+          gap: 0.5rem;
+          font-size: 1.5rem;
 
-                    span {
-                        font-size: 1.25rem;
-                    }
-                }
+          svg {
+            color: green;
+          }
 
-                .recipe__name {
-                    text-align: center;
-                    margin-bottom: 1rem;
-                    
-                    h1 {
-                        font-size: 2.25rem;
-                    }
-                }
-                
-                .recipe__author {
-                    text-align: center;
-                    margin-bottom: 1rem;
+          span {
+            font-size: 1.25rem;
+          }
+        }
 
-                    .author__name {
-                        display: inline-block;
-                        text-transform: uppercase;
-                        text-decoration: none;
-                        color: var(--seal-brown);
-                        margin-bottom: .5rem;
-                        transition: .3s ease-in-out;
+        .recipe__name {
+          text-align: center;
+          margin-bottom: 1rem;
 
-                        &:hover {
-                            color: var(--hover-link);
-                        }
-                    }
+          h1 {
+            font-size: 2.25rem;
+          }
+        }
 
-                    span {
-                        display: block;
-                        font-size: .95rem;
-                    }
-                }
+        .recipe__author {
+          text-align: center;
+          margin-bottom: 1rem;
 
-                .cta {
-                    display: flex;
-                    align-items: center;
-                    gap: .25rem;
-                    padding: .5rem 1.25rem;
-                    border: 1px solid var(--seal-brown);
-                    border-radius: 3rem;
-                    background: transparent;
-                    color: var(--seal-brown);
-                    outline: none;
-                    font-size: .95rem;
-                    cursor: pointer;
-                    text-transform: uppercase;
-                    transition: .3s ease-in-out;
-                    margin-bottom: .5rem;
+          .author__name {
+            display: inline-block;
+            text-transform: uppercase;
+            text-decoration: none;
+            color: var(--seal-brown);
+            margin-bottom: 0.5rem;
+            transition: 0.3s ease-in-out;
 
-                    &:hover {
-                        background: var(--seal-brown);
-                        color: var(--color-gray);
-                    }
-                }
-
-                .rating {
-                    display: flex;
-                    align-items: center;
-                    gap: .5rem;
-
-                    &__stars {
-                        display: flex;
-                        gap: .25rem;
-                    }
-                }
+            &:hover {
+              color: var(--hover-link);
             }
+          }
 
-            &-right {
-                width: 50%;
-                height: 100%;
+          span {
+            display: block;
+            font-size: 0.95rem;
+          }
+        }
 
-                .image__container {
-                    width: 100%;
-                    height: 100%;
+        .cta {
+          display: flex;
+          align-items: center;
+          gap: 0.25rem;
+          padding: 0.5rem 1.25rem;
+          border: 1px solid var(--seal-brown);
+          border-radius: 3rem;
+          background: transparent;
+          color: var(--seal-brown);
+          outline: none;
+          font-size: 0.95rem;
+          cursor: pointer;
+          text-transform: uppercase;
+          transition: 0.3s ease-in-out;
+          margin-bottom: 0.5rem;
 
-                    img {
-                        width: 100%;
-                        height: 100%;
-                        object-fit: cover;
-                    }
+          &:hover {
+            background: var(--seal-brown);
+            color: var(--color-gray);
+          }
+        }
+
+        .rating {
+          display: flex;
+          align-items: center;
+          gap: 0.5rem;
+
+          &__stars {
+            display: flex;
+            gap: 0.25rem;
+          }
+        }
+      }
+
+      &-right {
+        width: 50%;
+        height: 100%;
+
+        .image__container {
+          width: 100%;
+          height: 100%;
+
+          img {
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+          }
+        }
+      }
+    }
+  }
+
+  hr {
+    width: 90%;
+    margin: 2rem auto;
+  }
+`;
+
+export const Body = styled.section`
+  position: relative;
+  width: 90%;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  gap: 1rem;
+  /* margin-top: 2rem; */
+
+  .body__inner {
+    width: 65%;
+    .recipe__duration {
+      margin-bottom: 1rem;
+
+      p {
+        display: flex;
+        align-items: center;
+        gap: 0.25rem;
+        font-weight: bold;
+
+        svg {
+          font-size: 1.45rem;
+        }
+      }
+    }
+
+    .recipe__description {
+      margin-bottom: 2rem;
+
+      p {
+        text-align: justify;
+        font-size: 1.15rem;
+      }
+    }
+
+    .ingredients {
+      max-width: 50%;
+
+      .header {
+        margin-bottom: 0.5rem;
+      }
+      span.servings {
+        display: inline-block;
+        padding: 0.5rem 1rem;
+        background: var(--seal-brown);
+        border-radius: 5rem;
+        color: var(--color-gray);
+        margin-bottom: 0.75rem;
+      }
+
+      ul {
+        margin: 0.75rem 0 1.85rem;
+
+        li {
+          list-style: none;
+          padding: 0.5rem 0;
+          border-bottom: 1px dashed var(--mid-gray);
+        }
+      }
+    }
+
+    .procedure {
+      div {
+        margin-bottom: 3rem;
+
+        span {
+          display: inline-block;
+          font-size: 1.15rem;
+          font-weight: bold;
+          margin-bottom: 0.5rem;
+        }
+
+        p {
+          font-size: 1.15rem;
+          text-align: justify;
+        }
+      }
+    }
+
+    .rating {
+      .noAuth {
+        padding: 1rem;
+        text-align: center;
+        background: var(--seal-brown);
+        border-radius: 5px;
+        margin-bottom: 1.5rem;
+
+        p {
+          font-size: 1.25rem;
+          color: var(--color-gray);
+
+          a {
+            /* text-decoration: none; */
+            color: var(--color-gray);
+            transition: 0.3s ease-in-out;
+
+            &:hover {
+              color: var(--mid-gray);
+            }
+          }
+        }
+      }
+
+      .rate {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        gap: 0.85rem;
+        font-size: 1.25rem;
+
+        div {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          justify-content: center;
+
+          .stars {
+            display: block;
+            margin-bottom: 0.85rem;
+
+            svg {
+                cursor: pointer;
+                margin-right: .25rem;
+            }
+          }
+
+          button {
+            display: flex;
+            align-items: center;
+            gap: 0.25rem;
+            padding: 0.5rem 1.25rem;
+            border: 1px solid var(--seal-brown);
+            border-radius: 3rem;
+            background: transparent;
+            color: var(--seal-brown);
+            outline: none;
+            font-size: 0.95rem;
+            cursor: pointer;
+            text-transform: uppercase;
+            transition: 0.3s ease-in-out;
+            margin-bottom: 0.5rem;
+
+            &:hover {
+              background: var(--seal-brown);
+              color: var(--color-gray);
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const Comments = styled.section`
+  padding: 1rem 1.15rem;
+  border: 1px solid var(--seal-brown);
+  border-radius: 5px;
+  margin: 2rem 0;
+
+  .header {
+    margin-bottom: 0.85rem;
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+
+    svg {
+      font-size: 1.5rem;
+    }
+
+    span {
+      font-size: 1.25rem;
+      font-weight: bold;
+    }
+  }
+
+  .comment {
+    display: flex;
+    margin-bottom: 1.25rem;
+    
+    .user__avatar-container {
+      height: 50px;
+      width: 50px;
+      border-radius: 50%;
+
+      img {
+        height: 40px;
+        width: 40px;
+        border-radius: 50%;
+        object-fit: cover;
+      }
+    }
+
+    .comment__info {
+        .name {
+            span {
+                font-size: 1.12rem;
+
+                &.display__name {
+                    margin-right: .5rem;
+                }
+
+                &.username {
+                    color: #939290;
                 }
             }
         }
+
+        .timestamp {
+            margin-bottom: .25rem;
+
+            span {
+                font-size: .9rem;
+                color: #939290;
+            }
+        }
+    }
+  }
+
+  .add__comment {
+    display: flex;
+    /* align-items: center; */
+
+    .user__avatar-container {
+      height: 50px;
+      width: 50px;
+      border-radius: 50%;
+
+      img {
+        height: 40px;
+        width: 40px;
+        border-radius: 50%;
+        object-fit: cover;
+      }
     }
 
-    hr {
-        width: 90%;
-        margin: 1rem auto;
-    }
-`
+    .text__area {
+      width: 100%;
 
-export const Body = styled.section`
-    
-`
+      &-inner {
+        width: 100%;
+        padding: 0.25rem 0.65rem;
+        border: 1px solid var(--seal-brown);
+        border-radius: 4rem;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+
+        textarea {
+          width: 92%;
+          background: none;
+          border: none;
+          /* overflow: hidden; */
+          overflow-wrap: break-word;
+          outline: none;
+          font-family: var(--primary-font);
+          resize: none;
+
+          ::placeholder {
+            font-family: var(--primary-font);
+          }
+        }
+
+        button {
+          background: none;
+          border: none;
+          outline: none;
+          font-size: 1.5rem;
+          color: var(--seal-brown);
+          cursor: pointer;
+          transition: 0.3 ease-in-out;
+
+          &:hover {
+            color: var(--hover-link);
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const Sidebar = styled.section`
+  position: sticky;
+  padding: 1rem;
+  width: 30%;
+  min-height: 120px;
+  max-height: 130px;
+  border: 1px solid var(--mid-gray);
+  background: var(--color-gray);
+  border-radius: 0.85rem;
+  box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+
+  .btn {
+    display: block;
+    width: 100%;
+    margin-bottom: 0.5rem;
+    padding: 0.75rem 0;
+    border-radius: 4rem;
+    outline: none;
+    border: none;
+    text-transform: uppercase;
+    border: 1px solid var(--seal-brown);
+    color: var(--seal-brown);
+    font-size: 1rem;
+    cursor: pointer;
+    transition: 0.2s ease;
+
+    &:hover {
+      transform: scale(1.02);
+    }
+  }
+`;
--- src/pages/recipe display/Recipe.tsx
+++ src/pages/recipe display/Recipe.tsx
@@ -3,11 +3,13 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 
 // Styles
-import { Body, Wrapper } from "./Recipe.styles";
+import { Body, Comments, Sidebar, Wrapper } from "./Recipe.styles";
 
 // Icons
 import { MdOutlineBookmarkAdd, MdVerified } from "react-icons/md";
 import { FaRegStar } from "react-icons/fa";
+import { IoChatboxOutline } from "react-icons/io5";
+import { AiOutlineSend, AiOutlineClockCircle } from "react-icons/ai";
 
 // interface IAppProps {
 // }
@@ -65,24 +67,152 @@ const Recipe: React.FunctionComponent<IAppProps> = (props) => {
         <div className="body__inner">
           <div className="recipe__duration">
             <p>
-              Total Time <span>45 minutes</span>
+              <AiOutlineClockCircle /> <span>45 minutes</span>
             </p>
           </div>
 
           <div className="recipe__description">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut
-            temporibus odit accusamus impedit qui omnis aut labore consectetur
-            ipsum saepe praesentium asperiores in iste optio consequuntur
-            dolores, atque quaerat voluptatum quis incidunt odio neque vero quo!
-            In ea magnam nesciunt autem omnis iste sit eum modi sed provident
-            quidem distinctio, nihil voluptas rerum impedit eos neque quae odio.
-            Eveniet aliquam fuga recusandae natus iusto voluptatum reprehenderit
-            architecto temporibus cumque voluptate excepturi repudiandae dicta
-            autem odit porro, omnis vero rerum adipisci atque, exercitationem
-            cum nostrum minima! Tenetur ipsam velit aspernatur non molestias
-            labore neque nesciunt natus eos. Beatae sed totam aliquam?
+            <div className="header">
+              <h3>Recipe Description</h3>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut
+              temporibus odit accusamus impedit qui omnis aut labore consectetur
+              ipsum saepe praesentium asperiores in iste optio consequuntur
+              dolores, atque quaerat voluptatum quis incidunt odio neque vero
+              quo! In ea magnam nesciunt autem omnis iste sit eum modi sed
+              provident quidem distinctio, nihil voluptas rerum impedit eos
+              neque quae odio. Eveniet aliquam fuga recusandae natus iusto
+              voluptatum reprehenderit architecto temporibus cumque voluptate
+              excepturi repudiandae dicta autem odit porro, omnis vero rerum
+              adipisci atque, exercitationem cum nostrum minima! Tenetur ipsam
+              velit aspernatur non molestias labore neque nesciunt natus eos.
+              Beatae sed totam aliquam?
+            </p>
           </div>
+
+          <div className="ingredients">
+            <div className="header">
+              <h3>Ingredients</h3>
+            </div>
+            <span className="servings">4-6 Servings</span>
+            <ul>
+              <li>6 garlic cloves</li>
+              <li>3 red Thai chiles, 2 coarsely chopped, 1 thinly sliced</li>
+              <li>1/2 piece of ginger, peeled, coarsely chopped</li>
+              <li>1 Tbsp, double-concentrated tomato paste</li>
+              <li>1 samll red onion, finely chopped</li>
+              <li>4 Tbsp. fresh lemon juice, divided</li>
+            </ul>
+          </div>
+
+          <div className="procedure">
+            <div>
+              <span>Step 1</span>
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias,
+                sit optio facere harum quae sapiente iure quas sed consectetur
+                possimus unde cupiditate suscipit atque numquam, minus officia
+                maxime id commodi reiciendis, eveniet deserunt iusto iste et
+                facilis! Cum, voluptatum aperiam.
+              </p>
+            </div>
+            <div>
+              <span>Step 1</span>
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias,
+                sit optio facere harum quae sapiente iure quas sed consectetur
+                possimus unde cupiditate suscipit atque numquam, minus officia
+                maxime id commodi reiciendis, eveniet deserunt iusto iste et
+                facilis! Cum, voluptatum aperiam.
+              </p>
+            </div>
+            <div>
+              <span>Step 1</span>
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias,
+                sit optio facere harum quae sapiente iure quas sed consectetur
+                possimus unde cupiditate suscipit atque numquam, minus officia
+                maxime id commodi reiciendis, eveniet deserunt iusto iste et
+                facilis! Cum, voluptatum aperiam.
+              </p>
+            </div>
+          </div>
+
+          {/* Rating */}
+          <div className="rating">
+            <div className="noAuth">
+              <p>
+                <Link to={{ pathname: "/signin" }}>Sign In</Link> or{" "}
+                <Link to={{ pathname: "/signup" }}>Sign Up</Link> to leave a
+                rating!
+              </p>
+            </div>
+            <div className="rate">
+              <span>How would you rate this recipe?</span>
+              <div>
+                <span className="stars">
+                  <FaRegStar />
+                  <FaRegStar />
+                  <FaRegStar />
+                  <FaRegStar />
+                  <FaRegStar />
+                </span>
+                <button>Submit Rating</button>
+              </div>
+            </div>
+          </div>
+
+          {/* Comments */}
+          <Comments>
+            <div className="header">
+              <IoChatboxOutline />
+              <span>Comments</span>
+            </div>
+            <div className="body">
+              <div className="comment">
+                <div className="user__avatar-container">
+                  <img
+                    src="../../../public/images/foodiesfeed.com_fried-egg-avocado-open-sandwich.jpg"
+                    alt="user avatar"
+                  />
+                </div>
+                <div className="comment__info">
+                    <div className="name">
+                        <span className="display__name">Gudnix Great</span>
+                        <span className="username">@theweirdchef</span>
+                    </div>
+                    <div className="timestamp">
+                        <span>40 years ago</span>
+                    </div>
+                    <div className="comment__text">
+                        This looks great!
+                    </div>
+                </div>
+              </div>
+              <div className="add__comment">
+                <div className="user__avatar-container">
+                  <img
+                    src="../../../public/images/pexels-mo-eid-11798029.jpg"
+                    alt=""
+                  />
+                </div>
+                <div className="text__area">
+                  <div className="text__area-inner">
+                    <textarea rows="1" placeholder="Add a comment" />
+                    <button>
+                      <AiOutlineSend />
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Comments>
         </div>
+        <Sidebar>
+          <button className="btn share">Share</button>
+          <button className="btn share">Print</button>
+        </Sidebar>
       </Body>
     </>
   );
